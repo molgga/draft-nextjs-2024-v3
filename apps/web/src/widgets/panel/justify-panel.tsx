de3874181bfaee7a8a236f3a7d3ee9527720925a
molgga84 <molgga84@gmail.com>
2024-11-01 11:31:20 +0900

    fix 컴포넌트 props

--- apps/web/src/widgets/panel/justify-panel.tsx
+++ apps/web/src/widgets/panel/justify-panel.tsx
@@ -10,43 +10,36 @@ interface JustifyPanelProps extends React.HTMLAttributes<HTMLDivElement> {
   centerFlex?: number;
 }
 
-export const JustifyPanel = forwardRef<HTMLDivElement, JustifyPanelProps>(
-  ({ className, ...props }, ref) => {
-    const {
-      aside,
-      asideFlex = 1,
-      bside,
-      bsideFlex = 1,
-      children,
-      centerFlex = 1,
-      ...attrs
-    } = props;
+export function JustifyPanel({ className, ...props }: JustifyPanelProps) {
+  const {
+    aside,
+    asideFlex = 1,
+    bside,
+    bsideFlex = 1,
+    children,
+    centerFlex = 1,
+    ...attrs
+  } = props;
 
-    return (
-      <div
-        ref={ref}
-        className={clsx('ui-flex ui-mt-4 ui-items-center', className)}
-        {...attrs}
-      >
-        <div
-          className={clsx('ui-flex ui-justify-start', `ui-flex-${asideFlex}`)}
-        >
-          {aside}
-        </div>
-        <div
-          className={clsx(
-            'ui-flex ui-flex ui-justify-center',
-            `ui-flex-${centerFlex}`
-          )}
-        >
-          {children}
-        </div>
-        <div className={clsx('ui-flex ui-justify-end', `ui-flex-${bsideFlex}`)}>
-          {bside}
-        </div>
+  return (
+    <div
+      className={clsx('ui-flex ui-mt-4 ui-items-center', className)}
+      {...attrs}
+    >
+      <div className={clsx('ui-flex ui-justify-start', `ui-flex-${asideFlex}`)}>
+        {aside}
       </div>
-    );
-  }
-);
-
-JustifyPanel.displayName = 'JustifyPanel2';
+      <div
+        className={clsx(
+          'ui-flex ui-flex ui-justify-center',
+          `ui-flex-${centerFlex}`
+        )}
+      >
+        {children}
+      </div>
+      <div className={clsx('ui-flex ui-justify-end', `ui-flex-${bsideFlex}`)}>
+        {bside}
+      </div>
+    </div>
+  );
+}
