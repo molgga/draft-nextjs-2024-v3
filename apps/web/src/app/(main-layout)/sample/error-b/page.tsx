b11d657f47a84be6c4f99fdc0bb2de3c3cf492f5
molgga84 <molgga84@gmail.com>
2024-11-01 18:06:13 +0900

    fix: error 페이지

--- apps/web/src/app/(main-layout)/sample/error-b/page.tsx
+++ apps/web/src/app/(main-layout)/sample/error-b/page.tsx
@@ -1,6 +1,6 @@
 import { ErrorTest1 } from '@web/features/sample/ui/error-test1';
 
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic'; // 강제 오류 낼 거라 정적 렌더 안되게 함
 
 export default function Page() {
   return <ErrorTest1 />;
