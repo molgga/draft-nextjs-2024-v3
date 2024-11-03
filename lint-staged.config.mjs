31991b14fc1e4edf86f66664eb844615b54efbfb
molgga84 <molgga84@gmail.com>
2024-11-03 10:44:31 +0900

    husky 추가

---
+++ lint-staged.config.mjs
@@ -0,0 +1,5 @@
+export default {
+  // 'src/**/*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': ['biome format --write --no-errors-on-unmatched'],
+  // 'src/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': 'oxlint --fix',
+  'src/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+};
