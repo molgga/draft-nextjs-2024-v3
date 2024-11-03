885d9f37ce8debcf18d532b1144e24731e25364d
molgga84 <molgga84@gmail.com>
2024-11-03 13:56:08 +0900

    커밋 테스트

--- apps/web/src/shared/ui/page-title.tsx
+++ apps/web/src/shared/ui/page-title.tsx
@@ -4,7 +4,7 @@ interface PageTitleProps {
 
 export function PageTitle({ title }: PageTitleProps) {
   return (
-    <div className="ui-pt-3 ui-pb-8" foo="" a="">
+    <div className="ui-pt-3 ui-pb-8">
       <h1 className="ui-text-lg ui-font-semibold">{title}</h1>
     </div>
   );
