0e1f516f433d96f917dc0678c34dc77f75643a78
molgga84 <molgga84@gmail.com>
2024-11-03 10:51:24 +0900

    커밋 테스트1

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,6 @@
 export default {
   // 'src/**/*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': ['biome format --write --no-errors-on-unmatched'],
   // 'src/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': 'oxlint --fix',
-  'src/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  // 'src/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  'apps/web/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
 };
