9bc74a4f9245045c7a0ebea1eebb16d3ddd9c489
molgga84 <molgga84@gmail.com>
2024-11-03 14:17:35 +0900

    chore(docs): 메세지 UpperCase 테스트3

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,9 +2,6 @@ import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-case': [2, 'always', ['lower-case']],
-  },
 };
 
 export default config;
